1be3ef3ccef5807b8b77eca9b46c9a608258e400
Hannahjoansong <55770942+Hannahjoansong@users.noreply.github.com>
2019-10-21 01:03:25 -0700

    adding paragraph breaks and processing and another heading

--- homepage/processing/project_3/sketch.js
+++ homepage/processing/project_3/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
-  // put setup code here
-}
+  createCanvas(800, 600);
+  }
 
 function draw() {
-  // put drawing code here
-}
+  c = color(100, 100, 200);
+  background(0);
+  strokeJoin(ROUND);
+  fill(255, 255, 0);
+  arc(190, 60, 80, 80, 26, radians(325));
+  fill(253);
+  ellipse(200, 40, 10, 10);
+  fill(253, 0, 0);
+  arc(390, 60, 80, 80, radians(45), radians(225));
+  strokeWeight(3);
+  
+  
+ 
+  }
+  
