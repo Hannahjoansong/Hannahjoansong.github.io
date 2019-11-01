6fd8d3373405814ac6bf6a98dd6c276c58cc26e7
Hannahjoansong <55770942+Hannahjoansong@users.noreply.github.com>
2019-10-31 17:04:42 -0700

    ghost moving

--- homepage/processing/project_4/sketch.js
+++ homepage/processing/project_4/sketch.js
@@ -1,7 +1,31 @@
 function setup() {
-  // put setup code here
+  createCanvas(900, 600);
+  
 }
 
 function draw() {
-  // put drawing code here
-}
+  background(0);
+  if (mouseIsPressed) {
+    c = color(random(0,255), random(0,255), random(0,255));
+  fill(c);
+  } else {
+  fill(0, 100, 200);
+  }
+  fill(255, 150, 82);
+  arc(mouseX + 100, mouseY + 13, 81, 81, PI, TWO_PI, CHORD);
+  noStroke();
+  rect(mouseX + 65, mouseY + 10, 75, 20);
+  triangle(mouseX + 60, mouseY + 10, mouseX + 60, mouseY + 55, mouseX + 100, mouseY + 10);
+  triangle(mouseX + 85, mouseY + 30, mouseX + 100, mouseY + 55, mouseX + 115, mouseY + 30);
+  triangle(mouseX + 120, mouseY + 30, mouseX + 140, mouseY + 55, mouseX + 140, mouseY + 30);
+  fill(253);
+  ellipse(mouseX + 90, mouseY, 15, 25);
+  ellipse(mouseX + 120, mouseY, 15, 25);
+  fill(0, 0, 255);
+  circle(mouseX + 93, mouseY, 10);
+  circle(mouseX + 123, mouseY, 10);
+
+  }
+
+
+
